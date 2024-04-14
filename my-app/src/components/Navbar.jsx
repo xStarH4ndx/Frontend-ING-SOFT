19df8da0b160415132efd4d27dda86dde5da7140
broken_Arm <brunotoro1799@gmail.com>
2024-04-14 17:16:25 -0400

    s

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import Logo from '../assets/image/logo-empresa.png'
+
 
 function Navbar() {
   return (
     <div>
       <NavContainer>
+        <img src={Logo}/>
         <h2>English<span>WorkShop</span></h2>
         <div>
           <a href="/">Inicio</a>
@@ -12,7 +15,7 @@ function Navbar() {
           <a href="/">Precios</a>
           <a href="/">Sobre Mi</a>
 
-          <button>Sing up</button>
+          <button>Iniciar Sesión</button>
         </div>
       </NavContainer>
     </div>
@@ -22,43 +25,44 @@ function Navbar() {
 export default Navbar
 
 const NavContainer = styled.nav`
-background-color: #fff;
-position: sticky;
-top: 0;
-padding: 0 85px;
-display: flex;
-align-items: center;
-justify-content: space-between;
-height: 80px;
-box-shadow: 0 5px 80px rgba(0, 0, 0, 0.205);
-
-h2{
-    font-weight: 400;
-    span{
-        font-weight: bold;
-    }
-}
-
-a{
-  font-family: 'Poppins', sans-serif;
-  color: #000000;
-  position: relative;
-  text-decoration: none;
-  margin-right: 1rem;
-}
-
-button{
-  padding: 0.5rem 0.5rem;
-  font-size: 0.7rem;
-  color: #000000;
   background-color: #fff;
-  border-radius: 9px;
-  cursor: pointer;
-  transition: all 0.3s ease;
-}
+  position: sticky;
+  top: 0;
+  padding: 0 85px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 80px;
+  box-shadow: 0 5px 80px rgba(0, 0, 0, 0.205);
 
-button:hover{
-  background-color: #000000;
-  color: #fff
-}
+  h2{
+      font-weight: 400;
+      span{
+          font-weight: bold;
+      }
+  }
+
+  a{
+    font-family: 'Poppins', sans-serif;
+    color: #000000;
+    position: relative;
+    text-decoration: none;
+    margin-right: 1rem;
+  }
+
+  button{
+    padding: 0.5rem 0.5rem;
+    font-size: 0.7rem;
+    color: #000000;
+    background-color: #fff;
+    border-radius: 9px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+  }
+
+  button:hover{
+    background-color: #000000;
+    color: #fff
+  }
+
 `
