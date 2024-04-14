221190f933d6061f53506c1be0a549cd416f0132
xStarH4ndx <brunotoro1799@gmail.com>
2024-04-14 16:56:11 -0400

    casd

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -6,6 +6,14 @@ function Navbar() {
     <div>
       <NavContainer>
         <h2>English<span>WorkShop</span></h2>
+        <div>
+          <a href="/">Inicio</a>
+          <a href="/">Caracteristicas</a>
+          <a href="/">Precios</a>
+          <a href="/">Sobre Mi</a>
+
+          <button>Sing up</button>
+        </div>
       </NavContainer>
     </div>
   )
@@ -14,19 +22,43 @@ function Navbar() {
 export default Navbar
 
 const NavContainer = styled.nav`
-    h2{
-        font-weight: 400;
-        span{
-            font-weight: bold;
-        }
+background-color: #fff;
+position: sticky;
+top: 0;
+padding: 0 85px;
+display: flex;
+align-items: center;
+justify-content: space-between;
+height: 80px;
+box-shadow: 0 5px 80px rgba(0, 0, 0, 0.205);
+
+h2{
+    font-weight: 400;
+    span{
+        font-weight: bold;
     }
-    background-color: #fff;
-    position: sticky;
-    top: 0;
-    padding: 0 85px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    height: 80px;
-    box-shadow: 0 5px 80px rgba(0, 0, 0, 0.205);
+}
+
+a{
+  font-family: 'Poppins', sans-serif;
+  color: #000000;
+  position: relative;
+  text-decoration: none;
+  margin-right: 1rem;
+}
+
+button{
+  padding: 0.5rem 0.5rem;
+  font-size: 0.7rem;
+  color: #000000;
+  background-color: #fff;
+  border-radius: 9px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+}
+
+button:hover{
+  background-color: #000000;
+  color: #fff
+}
 `
