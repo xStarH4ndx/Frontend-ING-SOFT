ab9ff08f7dc4b5ff2f93ef134263698f96a9ebb2
broken_Arm <brunotoro1799@gmail.com>
2024-04-14 22:20:42 -0400

    Navbar fiinish

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import Logo from '../assets/image/logo-empresa.png';
+import BurgerButton from './BurgerButton';
 
 function Navbar() {
+
+  const [clicked, setClicked] = useState(false)
+  const handleClick = () =>{
+    //cuando está true lo pasa a false y vice versa
+    setClicked(!clicked)
+  }
   return (
     <NavContainer>
       <div>
         <img src={Logo} alt="Logo de la empresa" />
         <h2>English<span>WorkShop</span></h2>
       </div>
-      <div>
+      <div className={`links ${clicked ? 'active' :''} `}>
         <a href="/">Inicio</a>
         <a href="/">Caracteristicas</a>
         <a href="/">Precios</a>
         <a href="/">Sobre Mi</a>
-      </div>
-      <div>
         <button>Iniciar Sesión</button>
       </div>
+      <div className='burger'>
+        <BurgerButton clicked={clicked} handleClick={handleClick}/>
+      </div>
+      <BgDiv className={`initial ${clicked ? 'active' : ''}`}></BgDiv>
     </NavContainer>
   );
 }
 
 export default Navbar;
+
+const BgDiv = styled.div`
+  background-color: #17ed;
+  position: absolute;
+  top: -700px;
+  left: -2000;
+  transition:all 0.6s ease;
+  z-index: -1;
+  &.active{
+    top: 0;
+    left: 0;
+    width: 100%;
+    margin-top:5.63rem;
+    height: 300%;
+    z-index: -1;
+  }
+`
 
 const NavContainer = styled.nav`
   background-color: #fff;
@@ -35,6 +61,7 @@ const NavContainer = styled.nav`
   height: 90px;
   box-shadow: 0 5px 80px rgba(0, 0, 0, 0.205);
 
+  
   h2 {
     font-size: 2rem;
     font-weight: 400;
@@ -50,7 +77,7 @@ const NavContainer = styled.nav`
     position: relative;
     text-decoration: none;
     margin-right: 3rem;
-    aling-items:centes;
+    align-items:center;
   }
 
   button {
@@ -72,10 +99,62 @@ const NavContainer = styled.nav`
     width: 100%;
     display: felx;
     max-width: 70px;
+    margin-left: -30px;
   }
 
   div:first-child {
     display: flex;
     align-items: center;
+  }
+
+  .links{
+    position: absolute;
+    top: -700px;
+    left: -2000px;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    transition: all 0.5s ease;
+    a{
+      display: block;
+    }
+
+    @media(min-width:1190px){
+      position: initial;
+      margin: 0;
+      a{
+        display: inline;
+      }
+      display: block;
+    }
+  }
+
+  .links.active{
+    width: 100%;
+    display: block;
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    top: 100%;
+    left: 0;
+    right: 0;
+    text-align: center;
+    a{
+      font-weight: bold;
+      margin-left:0;
+      margin-right:0;
+      margin-top:1rem;
+    }
+    button{
+      margin-top:1rem;
+    }
+  }
+
+  .burger{
+    margin-left: 100px;
+    @media(min-width:1190px){
+      display: none;
+    }
   }
 `
